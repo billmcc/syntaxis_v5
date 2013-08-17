4f32b2c05d42d8313b8228c638bc6a0b9d07701e
Bill McCarvell <bill@syntaxis.net>
2013-08-17 14:00:53 -0400

    Initial commit

---
+++ syntaxis_v5/WebFolder/admin.waPage/scripts/index.js
@@ -0,0 +1,37 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var login1 = {};	// @login
+	var documentEvent = {};	// @document
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	login1.logout = function login1_logout (event)// @startlock
+	{// @endlock
+		window.location = '';
+	};// @lock
+
+	login1.login = function login1_login (event)// @startlock
+	{// @endlock
+		window.location = '';
+	};// @lock
+
+	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
+	{// @endlock
+		if(WAF.directory.currentUser() != null){
+			//take action for a logged in request
+			$$("main").loadComponent("/components/app.waComponent");
+		} else {
+			//take action for a non logged in request
+			$$("main").loadComponent("/components/public.waComponent");
+		}
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("login1", "logout", login1.logout, "WAF");
+	WAF.addListener("login1", "login", login1.login, "WAF");
+	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
+// @endregion
+};// @endlock
